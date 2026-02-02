1bc7c7eb72e0b851435ba9b655e9d181bde3b43d
Woomin-Wang <wwmin828@gmail.com>
2026-02-02 20:26:47 +0900

    chore: initialize project

---
+++ src/main/java/io/wisoft/prepair/prepair_api/PrepairApiApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.prepair.prepair_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepairApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepairApiApplication.class, args);
+	}
+
+}
