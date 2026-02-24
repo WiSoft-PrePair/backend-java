55bd413ca34561f2741e30e0906402723c7987b3
Boyeon-Shin <anna007096@gmail.com>
2026-02-24 16:30:51 +0900

    chore: 주석 삭제

--- src/main/java/io/wisoft/prepair/prepair_api/service/InterviewService.java
+++ src/main/java/io/wisoft/prepair/prepair_api/service/InterviewService.java
@@ -81,9 +81,6 @@ public class InterviewService {
         }
     }
 
-    /**
-     * [기업 맞춤 면접 질문 로직] - 현재 getSourceUrl() 저장 중, id 저장 논의 필요
-     */
     public List<InterviewQuestion> generateCompanyQuestions(UUID memberId, JobPosting jobPosting) {
         try {
             final String prompt = promptBuilder.buildCompanyQuestionPrompt(jobPosting.getContent());
